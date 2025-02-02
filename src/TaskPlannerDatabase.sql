559dead9b7c8b65d25c1464cfc671943f24f9f8c
yagmursabirli <yagmursabirli35@gmail.com>
2025-02-02 18:21:30 +0300

    Delete src/TaskPlannerDatabase.sql

--- src/TaskPlannerDatabase.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,         
-    task_name VARCHAR(255) NOT NULL,           
-    short_description TEXT,                    
-    category VARCHAR(100),                     
-    deadline DATE                               
-);
